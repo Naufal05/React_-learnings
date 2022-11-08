1bcdc8c55bb7a166b46b0447740117e066da5183
MOHAMED NAUFAL <mnaufalmz@gmail.com>
2022-11-09 01:27:37 +0530

    app.js and index.js

---
+++ classdemo/src/App.js
@@ -0,0 +1,17 @@
+import { Component } from "react";
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Content />
+        <Footer />
+      </div>
+    );
+  }
+}
+export default App;
